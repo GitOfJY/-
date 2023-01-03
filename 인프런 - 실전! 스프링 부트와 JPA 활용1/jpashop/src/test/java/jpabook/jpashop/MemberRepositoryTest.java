a85175ec75c8a3ebfc2db6cfcc5b7cef3328e826
JIYEON <104612045+GitOfJY@users.noreply.github.com>
2023-01-03 18:05:02 +0900

    Add files via upload

---
+++ 인프런 - 실전! 스프링 부트와 JPA 활용1/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
+        Assertions.assertThat(findMember).isEqualTo(member);
+        //System.out.println("findMember == member: " + (findMember == member));
+        //findMember == member: true
+        //같은 transaction 안에서 저장하면 영속성 context가 같음, 같은 영속성 context 안에서는 식별자가 같으면 같은 entity
+
+    }
+
+}
